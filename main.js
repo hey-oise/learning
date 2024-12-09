ead01dfcc9165c8c4592983aab23a2aef0542dbc
hey-oise <oise.giwa.okugbe@gmail.com>
2024-12-09 13:54:25 +0100

    init

--- main.js
+++ main.js
@@ -4,4 +4,15 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-let inventory ="stick";
+let inventory =["stick"];
+const button1 = document.querySelector("#button1")
+const button2 = documents.querySelector("#button2")
+const button3 = documents.querySelector("#button3")
+const text = documents.querySelector("#text")
+const xpText = documents.querySelector("#xpText")
+const healthText = documents.querySelector("#healthText")
+const goldText = documents.querySelector("#goldText")
+const monsterStats = documents.querySelector("#monsterStats")
+const monsterNameText = documents.querySelector("#monsterNameText")
+const monsterHealthText = documents.querySelector("#monsterHealthText")
+//inialize buttons
